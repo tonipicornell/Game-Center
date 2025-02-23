6d6b56b95617644c2dbe6b10edbaab05d0480df8
tonipicornell <tpicornell59@gmail.com>
2025-02-23 17:29:21 +0100

    CreateAccount creado

---
+++ app/src/main/java/com/example/gamecentertoni/CreateAccount.java
@@ -0,0 +1,56 @@
+package com.example.gamecentertoni;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateAccount extends AppCompatActivity {
+
+    private EditText personalNameUser;
+    private EditText surnamesUser;
+    private EditText dateOfBirthUser;
+    private EditText newUsername;
+    private EditText newPassword;
+    private EditText reEnterPassword;
+
+    private Button createAccountButton;
+
+    private TextView haveAccountText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_account);
+
+        personalNameUser = findViewById(R.id.enter_personal_name);
+        surnamesUser = findViewById(R.id.enter_surnames);
+        dateOfBirthUser = findViewById(R.id.enter_date_of_birth);
+        newUsername = findViewById(R.id.create_username);
+        newPassword = findViewById(R.id.create_password);
+        reEnterPassword = findViewById(R.id.enter_same_password);
+
+        createAccountButton = findViewById(R.id.new_account_button);
+
+        haveAccountText = findViewById(R.id.create_new_account);
+
+        haveAccountText.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateAccount.this, SignIn.class);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/gamecentertoni/SignIn.java
+++ app/src/main/java/com/example/gamecentertoni/SignIn.java
@@ -1,6 +1,10 @@
 package com.example.gamecentertoni;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignIn extends AppCompatActivity {
 
+    private EditText enterUser;
+    private EditText enterPassword;
+
+    private Button signInButton;
+
+    private TextView createAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+
+        enterUser = findViewById(R.id.enter_username);
+        enterPassword = findViewById(R.id.enter_password);
+        signInButton = findViewById(R.id.sign_in_button);
+        createAccount = findViewById(R.id.create_new_account);
+
+        // Si pulsas encima del texto te manda a la actividad CreateAccount
+        createAccount.setOnClickListener(v -> {
+            Intent intent = new Intent(SignIn.this, CreateAccount.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
